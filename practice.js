90a64b84631cf1b0a58dc7cfde9f0847afdab811
Adena Glasper <lefteye_50@Adenas-MacBook-Pro.local>
2022-08-16 17:29:01 -0700

    practiceQ

---
+++ practice.js
@@ -0,0 +1,7 @@
+$("#customer-table").append("<tr><td>Sam</td><td>Smith</td><td>sam.smith@gmail.com</td></tr>");
+$("#customer-table").append("<tr><td>Tom</td><td>Jones</td><td>tom.jones@gmail.com</td></tr>");
+
+// $(function () {
+//     $('#customer-table tr: last').after("<tr><td>Sam</td><td>Smith</td><td>sam.smith@gmail.com</td></tr>");
+// });
+
